326b8844e33ea59a6e6d1f4aafd154545b17a028
reagan <reaganm746@gmail.com>
2023-12-17 16:28:44 +0300

    event form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,45 @@
-// App.jsx
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container } from '@chakra-ui/react';
+import { Container, Box, ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
 import { Navbar } from './components/Navbar';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
-import { Checkout } from './pages/Checkout';
+import { Registration } from './pages/Registration';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: true,
+  },
+});
 
 function App() {
   return (
-    <>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       {/* Header */}
       <Header />
-      
+
       {/* Navbar */}
       <Navbar />
-      
+
       {/* Main Content */}
-      <Container maxW="container.lg" mt="4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="checkout" element={<Checkout />} />
-        </Routes>
+      <Container
+        maxW="container.lg"
+        mt="4"
+        p="4"
+        boxShadow="md"
+        rounded="lg"
+        bgGradient="linear(to-l, teal.200, purple.300)" // Use gradient background
+      >
+        <Box mb="4" textAlign="center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="registration" element={<Registration />} />
+          </Routes>
+        </Box>
       </Container>
-    </>
+    </ChakraProvider>
   );
 }
 
